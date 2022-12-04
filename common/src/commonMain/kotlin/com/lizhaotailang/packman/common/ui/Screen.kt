99df946577af3d08595de1415c3b77f6f68b9c3c
tonnyl <lizhaotailang@gmail.com>
2022-12-05 01:14:27 +0800

    Add pipeline schedule details screen

--- common/src/commonMain/kotlin/com/lizhaotailang/packman/common/ui/Screen.kt
+++ common/src/commonMain/kotlin/com/lizhaotailang/packman/common/ui/Screen.kt
@@ -9,12 +9,16 @@ sealed class Screen(val route: String) {
     object JobScreen :
         Screen(route = "job/{$ARG_JOB_ID}?$ARG_CANCELABLE={$ARG_CANCELABLE}&$ARG_RETRYABLE={$ARG_RETRYABLE}&$ARG_TRIGGERED={$ARG_TRIGGERED}")
 
+    object ScheduleScreen : Screen(route = "schedule/{$ARG_PIPELINE_SCHEDULE_ID}")
+
     companion object {
 
         const val ARG_JOB_ID = "arg_job_id"
         const val ARG_RETRYABLE = "arg_retryable"
         const val ARG_CANCELABLE = "arg_cancelable"
         const val ARG_TRIGGERED = "arg_triggered"
+
+        const val ARG_PIPELINE_SCHEDULE_ID = "arg_pipeline_schedule_id"
 
     }
 
