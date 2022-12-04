1c411b0cfca6d6b06933ddad3fafd4782ea26e8e
tonnyl <lizhaotailang@gmail.com>
2022-12-04 14:32:20 +0800

    Add code style check

--- buildSrc/build.gradle.kts
+++ buildSrc/build.gradle.kts
@@ -1,8 +1,29 @@
 repositories {
     google()
     mavenCentral()
+    gradlePluginPortal()
 }
 
 plugins {
     `kotlin-dsl`
+}
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+    targetCompatibility = JavaVersion.VERSION_1_8
+}
+
+dependencies {
+    implementation(libs.android.gradlePlugin)
+    implementation(libs.kotlin.gradlePlugin)
+    implementation(libs.spotless.gradlePlugin)
+}
+
+gradlePlugin {
+    plugins {
+        register("spotless") {
+            id = "com.lizhaotailang.packman.spotless"
+            implementationClass = "SpotlessConventionPlugin"
+        }
+    }
 }
---
+++ buildSrc/settings.gradle.kts
@@ -0,0 +1,14 @@
+@Suppress("UnstableApiUsage")
+
+dependencyResolutionManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        google()
+    }
+    versionCatalogs {
+        create("libs") {
+            from(files("../gradle/libs.versions.toml"))
+        }
+    }
+}
